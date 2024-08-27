050e6d5da7e54289ec4b3ba78840ceb0e4e51d24
MtJakhar <thvsd.10fingers@gmail.com>
2024-08-26 20:46:39 -0400

    first commit

---
+++ chapter2/chapter2.java
@@ -0,0 +1,11 @@
+//simple Java program
+//class header
+public class Simple {
+  //method header
+  //Java programs must have a main method
+  public static void main(String[] args) {
+    //Java statement
+    //only Java statements end with ;
+    System.out.printLn("Programming is great fun!");
+  }
+}
