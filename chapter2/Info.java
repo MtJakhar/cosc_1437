d96e4c1728cff78e5b158130d023b397c0ac16c6
MtJakhar <thvsd.10fingers@gmail.com>
2024-09-06 15:14:46 -0500

    homework 2.4

---
+++ chapter2/Info.java
@@ -0,0 +1,8 @@
+public class Info {
+  public static void main(String[] args) {
+    System.out.println("Muizz");
+    System.out.println("address");
+    System.out.println("city,state,zip");
+    System.out.println("telephone number");
+  }
+}
