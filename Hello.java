ab0c5e22adb29c737933b131f052f65e6efce848
MtJakhar <thvsd.10fingers@gmail.com>
2024-09-06 14:30:34 -0500

    homework 2.1

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
+//September 6
+
 public class Hello {
   public static void main(String[] args) {
     System.out.println("Hello World");
   }
 }
+
+//2.5 all java source code file names should end with .java
+//2.6 Every java application must have a method named main
---
+++ chapter2/Columbus.java
@@ -0,0 +1,7 @@
+public class Columbus {
+  public static void main(String[] args) {
+    System.out.println("In 1492 Columbus sailed the ocean blue.");
+  }
+}
+//When this code is saved to a file it must be named Columbus.java
+
