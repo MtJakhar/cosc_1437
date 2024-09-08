a611f42265bd63d16059c699082a47739ab9baf6
MtJakhar <thvsd.10fingers@gmail.com>
2024-09-08 16:12:28 -0500

    assignment 1

---
+++ chapter2/Assignment1.java
@@ -0,0 +1,25 @@
+// a program which prints game directions to the terminal
+// Muizz Jakhar 
+// September 8 2024
+public class Assignment1 {
+  //this version has the same number of characters as in the Assignment1 Rubric but it looks weird in the terminal
+  public static void main(String[] args) {
+    System.out.println("-------------------------------------------------------");
+    System.out.println("|--------Welcome to the COSC-1437--------|");
+    System.out.println("|------------Classic Text Based-----------|");
+    System.out.println("|----------Naval Combat Game!------------|");
+    System.out.println("-------------------------------------------------------");
+  }
+}
+// the version below has a different number of characters so that it looks good in the terminal
+/*
+ public class Assignment1 {
+   public static void main(String[] args) {
+     System.out.println("------------------------------------------");
+     System.out.println("|--------Welcome to the COSC-1437--------|");
+     System.out.println("|-----------Classic Text Based-----------|");
+     System.out.println("|----------Naval Combat Game!------------|");
+     System.out.println("------------------------------------------");
+   }
+ }
+ */
