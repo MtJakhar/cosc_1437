f4f35806b5d53aab8290ae833dd212615967ac5d
MtJakhar <thvsd.10fingers@gmail.com>
2024-10-09 12:58:24 -0500

    primative datatypes

--- javaOnlineCourse/sectionOne/HelloWorld.java
+++ javaOnlineCourse/sectionOne/HelloWorld.java
@@ -1,8 +1,22 @@
-class HellowWorld {
+class HelloWorld {
   public static void main(String[] args) {
     int age = 27;
 
-    
     System.out.println("I am " + age + " years old.");
+
+    //all primative data types
+    byte aSingleByte = 100;
+    short aSmallNumber = 2000;
+    int anInteger = 1230123;
+    long aLargeNumber = 223413123234234L;
+
+    double aDouble = 1.234;
+    float aFloat = 4.345F;
+
+    boolean isTrue = true;
+    boolean isFalse = false;
+
+    char characterHere = 'k';
+
   }
 }
