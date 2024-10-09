021c3d287cb228218cc5c571673e98a0c0b5e236
MtJakhar <thvsd.10fingers@gmail.com>
2024-10-09 12:50:49 -0500

    Java review

---
+++ javaOnlineCourse/Main.java
@@ -0,0 +1,6 @@
+//class must match source file name
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
---
+++ javaOnlineCourse/sectionOne/HelloWorld.java
@@ -0,0 +1,8 @@
+class HellowWorld {
+  public static void main(String[] args) {
+    int age = 27;
+
+    
+    System.out.println("I am " + age + " years old.");
+  }
+}
