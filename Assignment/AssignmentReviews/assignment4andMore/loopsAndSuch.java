9f01ab803a65366121b08d3cea96ef5120bdf0c6
MtJakhar <thvsd.10fingers@gmail.com>
2024-12-05 07:51:57 -0600

    whileLoop

---
+++ Assignment/AssignmentReviews/assignment4andMore/loopsAndSuch.java
@@ -0,0 +1,27 @@
+public class loopsAndSuch {
+  public static void main(String[] args) {
+  }
+
+  public void IncrementDecrement() {
+    int num = 4;
+    System.out.println("number is " + num);
+    System.out.println("I will increment number.");
+
+    num++;
+
+    System.out.println("Now, number is " + num);
+    System.out.println("I Will decrement num.");
+
+    num--;
+    System.out.println("Now, number is " + num);
+  }
+
+  public void WhileLoop() {
+    int number = 1;
+    while (number <= 5) {
+      System.out.println("Hello");
+      number++;
+    }
+    System.out.println("Thats all folks!");
+  }
+}
